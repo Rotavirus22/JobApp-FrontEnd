0a5561ea1ffd676e0d516a0cbf44cdff617b8246
Rohan Pokhrel <rohanpokhrel7@gmail.com>
2024-02-03 22:08:04 +0545

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: AppTheme().lightTheme,
-      initialRoute: "/register",
+      initialRoute: "/login",
       navigatorKey: navigagorKey,
       routes: {
         "/": (context) => ChangeNotifierProvider(
